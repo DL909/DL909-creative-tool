2487fcf24600d3dff980abd5d70dee6594e5e14e
DL909 <matthewDingL@outlook.com>
2024-02-05 11:02:27 +0800

    add timer block3

--- src/main/java/net/dl909/dl_ct/block/item_stream_emulator_block.java
+++ src/main/java/net/dl909/dl_ct/block/item_stream_emulator_block.java
@@ -10,7 +10,6 @@ import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.Properties;
-import net.minecraft.state.property.Property;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
