f6ec49784ac08f5544104418f93cbd0ae555559e
DL909 <matthewDingL@outlook.com>
2024-06-22 16:03:35 +0800

    removed crafter(which has been moved to https://github.com/DL909/Crafter)

--- src/main/java/dl909/dl_ct/client/DL909_creative_toolClient.java
+++ src/main/java/dl909/dl_ct/client/DL909_creative_toolClient.java
@@ -1,9 +1,6 @@
 package dl909.dl_ct.client;
 
-import dl909.dl_ct.dl909_creative_tool;
-import dl909.dl_ct.client.gui.screen.ingame.CrafterScreen;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.screenhandler.v1.ScreenRegistry;
 
 public class DL909_creative_toolClient implements ClientModInitializer {
     /**
@@ -11,7 +8,6 @@ public class DL909_creative_toolClient implements ClientModInitializer {
      */
     @Override
     public void onInitializeClient() {
-        ScreenRegistry.register(dl909_creative_tool.CRAFTER_3X3, CrafterScreen::new);
-        ScreenRegistry.register(dl909_creative_tool.BOX_SCREEN_HANDLER, dl909.dl_ct.client.gui.screen.ingame.BoxScreen::new);
+
     }
 }
