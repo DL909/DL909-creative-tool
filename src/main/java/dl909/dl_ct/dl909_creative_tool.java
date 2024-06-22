fa3b2b9e268976aef65e8e443fa6cb6f0c10a2c9
DL909 <matthewDingL@outlook.com>
2024-06-22 16:03:48 +0800

    removed crafter(which has been moved to https://github.com/DL909/Crafter)

--- src/main/java/dl909/dl_ct/dl909_creative_tool.java
+++ src/main/java/dl909/dl_ct/dl909_creative_tool.java
@@ -4,28 +4,18 @@ import dl909.dl_ct.block.*;
 import dl909.dl_ct.block.entity.*;
 import dl909.dl_ct.item.debug_key;
 import dl909.dl_ct.item.hopper_update_tool;
-import dl909.dl_ct.screen.BoxScreenHandler;
-import dl909.dl_ct.screen.CrafterScreenHandler;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
-import net.fabricmc.fabric.api.screenhandler.v1.ScreenHandlerRegistry;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
 import net.minecraft.block.Material;
-import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.item.*;
-import net.minecraft.network.NetworkThreadUtils;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.screen.ScreenHandlerType;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.BlockPos;
 
 public class dl909_creative_tool implements ModInitializer {
 
@@ -38,10 +28,6 @@ public class dl909_creative_tool implements ModInitializer {
         Registry.register(Registries.ITEM, new Identifier("dl_ct", "item_kill_block"), ITEM_KILL_BLOCK_ITEM);
         Registry.register(Registries.BLOCK, new Identifier("dl_ct", "item_stream_emulator_block"), ITEM_STREAM_EMULATOR_BLOCK);
         Registry.register(Registries.ITEM, new Identifier("dl_ct", "item_stream_emulator_block"), ITEM_STREAM_EMULATOR_BLOCK_ITEM);
-        Registry.register(Registries.BLOCK, new Identifier("minecraft","crafter"),CRAFTER_BLOCK);
-        Registry.register(Registries.ITEM,new Identifier("minecraft","crafter"),CRAFTER_BLOCK_ITEM);
-        Registry.register(Registries.BLOCK, new Identifier("dl_ct","box_block"),BOX_BLOCK);
-        Registry.register(Registries.ITEM,new Identifier("dl_ct","box_block"),BOX_BLOCK_ITEM);
 
         //Registry.register(Registries.BLOCK, new Identifier("dl_ct","test_light_block"),DL909_REDSTONE_LAMP);
         //Registry.register(Registries.ITEM, new Identifier("dl_ct","test_light_block"),new BlockItem(DL909_REDSTONE_LAMP,new Item.Settings()));
@@ -64,30 +50,7 @@ public class dl909_creative_tool implements ModInitializer {
             content.add(TOOLED_STICK);
             content.add(ICON);
             content.add(HOPPER_DEBUG_KEY);
-            content.add(BOX_BLOCK_ITEM);
         });
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.REDSTONE).register(content->{
-            content.add(CRAFTER_BLOCK_ITEM);
-        });
-
-        ServerPlayNetworking.registerGlobalReceiver(
-            SLOT_CHANGE_STATE,
-            (server, player, handler, buf, responseSender) -> {
-                BlockPos pos = buf.readBlockPos();
-                int slotId = buf.readInt();
-                int syncId = buf.readInt();
-                boolean enabled = buf.readBoolean();
-                CrafterScreenHandler crafterScreenHandler;
-                if (player.isSpectator() || syncId != player.currentScreenHandler.syncId) {
-                    return;
-                }
-                Object object = player.currentScreenHandler;
-                if (object instanceof CrafterScreenHandler && (object = (crafterScreenHandler = (CrafterScreenHandler)object).getInputInventory()) instanceof CrafterBlockEntity) {
-                    CrafterBlockEntity crafterBlockEntity = (CrafterBlockEntity)object;
-                    crafterBlockEntity.setSlotEnabled(slotId, enabled);
-                }
-            }
-        );
 
     }
 
@@ -116,21 +79,6 @@ public class dl909_creative_tool implements ModInitializer {
             FabricBlockEntityTypeBuilder.create(item_stream_emulator_block_entity::new, ITEM_STREAM_EMULATOR_BLOCK).build()
     );
 
-    public static final Block CRAFTER_BLOCK = new CrafterBlock(AbstractBlock.Settings.of(Material.STONE).strength(0.8f));
-    public static final Item CRAFTER_BLOCK_ITEM = new BlockItem(CRAFTER_BLOCK,new Item.Settings());
-    public static final BlockEntityType<CrafterBlockEntity> CRAFTER_BLOCK_ENTITY = Registry.register(
-            Registries.BLOCK_ENTITY_TYPE,
-            new Identifier("minecraft","crafter_block_entity"),
-            FabricBlockEntityTypeBuilder.create(CrafterBlockEntity::new,CRAFTER_BLOCK).build()
-    );
-
-    public static final Block BOX_BLOCK = new BoxBlock(FabricBlockSettings.copyOf(Blocks.CHEST));
-    public static final BlockItem BOX_BLOCK_ITEM = new BlockItem(BOX_BLOCK, new Item.Settings());
-    public static final BlockEntityType<BoxBlockEntity> BOX_BLOCK_ENTITY_BLOCK_ENTITY_TYPE = Registry.register(
-            Registries.BLOCK_ENTITY_TYPE,
-            new Identifier("dl_ct","box_block_entity"),
-            FabricBlockEntityTypeBuilder.create(BoxBlockEntity::new,BOX_BLOCK).build()
-    );
 
     //public static final Block DL909_REDSTONE_LAMP = new RedstoneLampBlock(AbstractBlock.Settings.of(Material.REDSTONE_LAMP).luminance(createLightLevelFromLitBlockState(15)).strength(0.3F).sounds(BlockSoundGroup.GLASS));
     //public static final Block TIMER_BLOCK = new timer_block(AbstractBlock.Settings.of(Material.STONE));
@@ -145,8 +93,6 @@ public class dl909_creative_tool implements ModInitializer {
     }
     */
 
-    public static final ScreenHandlerType<CrafterScreenHandler> CRAFTER_3X3 = ScreenHandlerRegistry.registerExtended(new Identifier("dl_ct", "general"), CrafterScreenHandler::new);
-    public static final ScreenHandlerType<BoxScreenHandler> BOX_SCREEN_HANDLER = ScreenHandlerRegistry.registerSimple(new Identifier("dl_ct", "test"), BoxScreenHandler::new);
     public static final ItemGroup ITEM = FabricItemGroup.builder(
                     new Identifier("dl_ct", "general")).
             icon(() -> new ItemStack(ICON)).build();
